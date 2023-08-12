b95c9054babb03a5b378f410cb5ae1c449d19f1b
Павел <claid07@mail.ru>
2023-08-12 08:57:56 +0400

    modify item.py create test

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,8 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+import pytest
+class TestItem:
+    """Тест класса Item"""
+def test_calculate_total_price():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.calculate_total_price() == 200000
