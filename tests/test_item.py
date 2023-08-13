de83815eb0ab74a4367e750d55e0bf29839a4444
Павел <claid07@mail.ru>
2023-08-13 10:12:53 +0400

    fix problems

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,8 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 from src.item import Item
+
 import pytest
 class TestItem:
     """Тест класса Item"""
+
+
 def test_calculate_total_price():
     item1 = Item("Смартфон", 10000, 20)
     assert item1.calculate_total_price() == 200000
+def test_apply_discount():
+    item1 = Item("Смартфон", 10000, 20)
+    assert item1.apply_discount() is None
